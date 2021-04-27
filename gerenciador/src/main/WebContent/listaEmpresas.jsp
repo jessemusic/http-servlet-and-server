69b46dabfa88bbf31c7bed7cd73d121ee707a73d
jessemusic <jmc_cursos@hotmail.com>
2021-04-27 15:51:52 -0300

    Melhorando o JSP

--- gerenciador/src/main/WebContent/listaEmpresas.jsp
+++ gerenciador/src/main/WebContent/listaEmpresas.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ page import="java.util.List, br.com.jmccursos.gerenciador.servlet.Empresa" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +8,18 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+	PrintWriter out = response.getWriter();
+		
+		<ul>
+		<%
+			List<Empresa> lista = (List<Empresa>)request.getAttribute("empresas");
+			for (Empresa empresa : lista) {
+		%>	
+			<li><%= empresa.getNome() %></li>
+		<%	
+			}
+		%>
+		</ul>
+		
 </body>
 </html>
--- gerenciador/src/main/java/br/com/jmccursos/gerenciador/servlet/ListaEmpresesasServlet.java
+++ gerenciador/src/main/java/br/com/jmccursos/gerenciador/servlet/ListaEmpresesasServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,19 +23,10 @@ public class ListaEmpresesasServlet extends HttpServlet {
 		
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
-		PrintWriter out = response.getWriter();
+		request.setAttribute("empresas", lista);
 		
-		out.println("<html><body>");
-		out.println("<ul>");
-		for (Empresa empresa : lista) {
-			out.println("<li>" + empresa.getNome() + "</li>");
-			
-		}
-		out.println("</ul>");
-		out.println("</body></html>");
-		
-		
-		
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/listaEmpresas.jsp");
+		requestDispatcher.forward(request, response);
 	}
 
 }
